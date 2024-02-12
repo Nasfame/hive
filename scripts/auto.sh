d9137a5faa047911a6fe8bb5c1cae2fe697a8d4d
Hiro <laciferin@gmail.com>
2024-02-13 02:22:57 +0530

    chore: add scripts for  release of multiple go mods (#112)
    * add: scripts from gotcha
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 6 minutes
    
    * fix: and remove gotcha
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes
    
    * hive: go.work
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes

---
+++ scripts/auto.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if ! git diff --quiet
+then
+    printf "working tree is not clean\n\n"
+    git status
+    exit 1
+fi
+
+echo "checking out master"
+
+git checkout master
+
+echo "updating repo"
+
+git pull origin master
+
+git describe --tags
+
+echo "what's the tag"
+
+read tag
+
+tag=$(echo "$tag" | sed 's/ //g')
+
+export TAG=$tag
+
+chmod +x scripts/release.sh
+chmod +x scripts/tag.sh
+
+./scripts/release.sh
+./scripts/tag.sh
+
+go work sync
+
+git add .
+git commit -m "chore: update deps-$tag"
+
+git push origin --tags
+git push
+
+echo "check the latest version, sleeping for 1 min for the go registry to sync up"
+
+sleep 1
+
+#Check
+
+go list -m -json github.com/CoopHive/hive@latest
+
+go list -m -json github.com/CoopHive/hive/exp/dealer/goczmqExampleDealer@latest
+#go run github.com/CoopHive/exp/basic@$tag
