175ebb4cc1061f536bff0dfedea1c484c837c3f0
Hiro <laciferin@gmail.com>
2024-03-16 05:24:50 +0530

    scripts: typechain
    Signed-off-by: Hiro <laciferin@gmail.com>

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -116,6 +116,13 @@ const config: HardhatUserConfig = {
   //     // Doesn't need an API key
   //     enabled: true
   // }
+  typechain: {
+    outDir: 'typechain-types',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false // defaults to false
+  },
 };
 
 import "./tasks";
